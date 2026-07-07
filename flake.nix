6fe0852d6e3cb2b2c1171a998335114e33eea44c
Casper Howse <casperhowse@gmail.com>
2026-07-07 13:11:15 +1200

    update to correct github username

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    cappaos.url = "github:CappaBot/CappaOS-Nix";
+    cappaos.url = "github:CappaBot1/CappaOS-Nix";
   };
 
   outputs = { self, nixpkgs, cappaos, ... }: {
